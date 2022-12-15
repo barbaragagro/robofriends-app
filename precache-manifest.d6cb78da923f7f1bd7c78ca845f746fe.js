9b3083a11c87ed4010d2bd70c1c87d5e1c81e028
barbaragagro <barbaragagro1@gmail.com>
2022-12-15 19:30:19 +0100

    Updates

---
+++ precache-manifest.d6cb78da923f7f1bd7c78ca845f746fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d63f385a0bea21208f8",
+    "url": "/robofriends-app/static/css/main.a976d050.chunk.css"
+  },
+  {
+    "revision": "3d63f385a0bea21208f8",
+    "url": "/robofriends-app/static/js/main.259befab.chunk.js"
+  },
+  {
+    "revision": "0203602790863f22d5f1",
+    "url": "/robofriends-app/static/js/runtime~main.7856564c.js"
+  },
+  {
+    "revision": "ea7e44556a5ff0ce7d1f",
+    "url": "/robofriends-app/static/css/2.c2ae979d.chunk.css"
+  },
+  {
+    "revision": "ea7e44556a5ff0ce7d1f",
+    "url": "/robofriends-app/static/js/2.b8a35cc6.chunk.js"
+  },
+  {
+    "revision": "dea95dd7d80549f94d6e",
+    "url": "/robofriends-app/static/js/3.89b26869.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "88a56fc60e45047f09d65969efc7b7bc",
+    "url": "/robofriends-app/index.html"
+  }
+];
